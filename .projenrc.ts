9f5779c1030c2c0fa5fbbb46dcb44c7c9911babf
Julian Michel <mail@julianmichel.de>
2024-06-04 22:15:00 +0200

    chore: enable prettier

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,15 @@
-import { awscdk } from 'projen';
+import { awscdk } from "projen";
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Julian Michel',
-  authorAddress: 'mail@julianmichel.de',
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.4.0',
-  name: 'my-dummy-construct',
+  author: "Julian Michel",
+  authorAddress: "mail@julianmichel.de",
+  cdkVersion: "2.1.0",
+  defaultReleaseBranch: "main",
+  jsiiVersion: "~5.4.0",
+  name: "my-dummy-construct",
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/mail/my-dummy-construct.git',
+  repositoryUrl: "https://github.com/jumic/my-dummy-construct.git",
+
+  prettier: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="my-dummy-construct.Hello"></a>
+
+#### Initializers <a name="Initializers" id="my-dummy-construct.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'my-dummy-construct'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#my-dummy-construct.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="my-dummy-construct.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
