362c538cf270e6cb468833012528b8a6e9650afa
Julian Michel <mail@julianmichel.de>
2024-06-05 08:07:27 +0200

    feat: release 1.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -15,6 +15,8 @@ const project = new awscdk.AwsCdkConstructLibrary({
   releaseToNpm: true,
   npmAccess: NpmAccess.RESTRICTED,
 
+  majorVersion: 1,
+
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
